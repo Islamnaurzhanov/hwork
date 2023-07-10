1352905d636840ce5ba36759ab537567dd085fb7
Islamnaurzhanov <137942318+Islamnaurzhanov@users.noreply.github.com>
2023-07-09 23:28:31 -0500

    Add files via upload

---
+++ My_homework1/MyHomework1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String texst = " Мой ";
+        String texst2 = " первый ";
+        String texst3 = " проект ";
+        String texst4 = texst + texst2 + texst3;
+
+        System.out.println(texst4);
+        System.out.println ("Поздраляю с первым созданным кодом Ислам !!!");
+
+
+    }
+}
---
+++ My_homework2/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        int term1 = 10 ;
+        int term2 = 15 ;
+        int result = term1 + term2 ;
+
+        int minuend = 256 ;
+        int subtrahend = 48 ;
+        int result2 = minuend + subtrahend ;
+
+
+        int number1 = 45 ;
+        int number2 = 97 ;
+        int result3 = number1 * number2 ;
+
+
+        int number4 = 596 ;
+        int number5 = 22 ;
+        int result4 = number4 / number5 ;
+
+
+        int number10 = 596 ;
+        int number11 = 22 ;
+        int result40 = number10 - ( number11 + subtrahend) + term1 ;
+
+
+
+        System.out.println ( result );
+        System.out.println ( result2 );
+        System.out.println ( result3 );
+        System.out.println ( result4 );
+        System.out.println ( result40 );
+
+
+
+    }
+}
